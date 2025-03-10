60641e3ddcad4fa4e06d9424c5b334e63e86f8b2
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-10 11:54:08 -0300

    v1.6

--- App.tsx
+++ App.tsx
@@ -3,10 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Product } from './src/components/product';
 
 const PRODUCT = {
-  imageURI: "",
-  title: "",
-  description: "",
-  price: "",
+  imageURI: "https://m.media-amazon.com/images/I/61FR1BJ71IL.jpg",
+  title: "Teclado Mecânico",
+  description: "YINDIAO Teclado mecânico para jogos K68 60% sem fio, Bluetooth 5.0/2.4 GHz, receptor USB-C 2 em 1 de modo duplo, troca a quente, layout compacto dos EUA de 68 teclas, economia de energia, interruptorYINDIAO Teclado mecânico para jogos K68 60% sem fio, Bluetooth 5.0/2.4 GHz, receptor USB-C 2 em 1 de modo duplo, troca a quente, layout compacto dos EUA de 68 teclas, economia de energia, interruptorYINDIAO Teclado mecânico para jogos K68 60% sem fio, Bluetooth 5.0/2.4 GHz, receptor USB-C 2 em 1 de modo duplo, troca a quente, layout compacto dos EUA de 68 teclas, economia de energia, interruptor",
+  price: "R$ 244,90",
 }
 
 export default function App() {
